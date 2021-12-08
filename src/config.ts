db8486b4b7b1462d9c4a21ec08810a15a24f06a9
Mario Reder <mreder1289@gmail.com>
2021-12-08 21:52:51 +0100

    set tweet id

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,10 @@
 // Change these variables on airdrop campaign launch
 export const DATE_THRESHOLD = new Date('2021-12-01');
-export const RETWEET_ID = '1402264239927185422'; //'1434868799040368641';
+export const RETWEET_ID = '1468678836632133638';
 
 // do not change these
 export const TWITTER_ACCOUNT_ID = '1415961588016816129';
 export const TWITTER_ACCOUNT_ID_HUMANGUILD = '1382487765430898688';
 export const TWITTER_ACCOUNT_ID_NEAR_GAMES = '1427725808626511877';
 export const TWITTER_ACCOUNT_ID_NEAR_PROTOCOL = '1031949518609121280';
-export const REQUIRED_RT_HASHTAGS = ['nft', 'playtoearn'];
-// export const REQUIRED_RT_HASHTAGS = ['nft', 'blockchaingaming', 'playtoearn'];
+export const REQUIRED_RT_HASHTAGS = ['nft', 'blockchaingaming', 'playtoearn'];
